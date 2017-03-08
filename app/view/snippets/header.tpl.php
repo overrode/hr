4c6981178b04dbad8fe45051d1a276b9bbbe0400
DaniSimon <overrode@icloud.com>
2017-03-08 14:43:45 +0200

    TI-3 - Refactoring html.

--- app/view/snippets/header.tpl.php
+++ app/view/snippets/header.tpl.php
@@ -7,18 +7,24 @@
     <script src="../js/jquery-3.1.1.js"></script>
     <script src="../js/custom.js"></script>
     <script src="../js/bootstrap.min.js"></script>
-	<title>
-		<?php if (isset($html_head_title)) : ?>
-			<?php echo $html_head_title; ?> |
-		<?php endif ?>
-	</title>
+    <title>
+        <?php if (isset($html_head_title)) : ?>
+            <?php echo $html_head_title; ?> |
+        <?php endif ?>
+    </title>
 </head>
 
 <body>
 
 <div class="container-fluid">
     <header class="row">
-        <div class="col-md-4 col-xs-12 text-center well" id="header_time-it"><i class="fa fa-clock-o" style="font-size: 25px;"></i><h1>TIME IT</h1></div>
-        <div class="col-md-4 col-xs-12 text-center pull-right well" id="header_user"><i class="fa fa-user-o" style="font-size: 25px;"></i><h1><?php echo "User"; ?></h1></div>
+        <div class="col-md-4 col-xs-12 text-center well" id="header_time-it">
+            <i class="fa fa-clock-o" style="font-size: 25px;"></i>
+            <h1>TIME IT</h1>
+        </div>
+        <div class="col-md-4 col-xs-12 text-center pull-right well" id="header_user">
+            <i class="fa fa-user-o" style="font-size: 25px;"></i>
+            <h1><?php echo "User"; ?></h1>
+        </div>
     </header>
 </div>
