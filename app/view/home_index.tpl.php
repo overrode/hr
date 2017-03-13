afce68b2246959940c4ea05b10af33d558689841
DaniSimon <overrode@icloud.com>
2017-03-13 15:40:59 +0200

    T-11 - Header buttons redesign.

--- app/view/home_index.tpl.php
+++ app/view/home_index.tpl.php
@@ -1,8 +1,16 @@
 <?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
 
 <main class="container-fluid">
-    <div class="row main_main"></div>
+    <div class="row main_main_top"></div>
     <button type="button" class="center-block btn btn-danger" id="main_button">START TRACKING</button>
+    <div id="gui"></div>
+    <div id="canvas-container">
+        <div id="mountains2"></div>
+        <div id="mountains1"></div>
+        <div id="skyline"></div>
+    </div>
+
+    <div class="main_main_bottom"></div>
 </main>
 
 <?php @include APP_PATH . 'view/snippets/footer.tpl.php'; ?>
