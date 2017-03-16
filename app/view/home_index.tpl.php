90073f3482b5fa1f9a1e2b32589fa570c869747a
DaniSimon <overrode@icloud.com>
2017-03-16 18:06:15 +0200

    TI-24 - Refactoring CSS/HTML to register and login forms.

--- app/view/home_index.tpl.php
+++ app/view/home_index.tpl.php
@@ -2,18 +2,14 @@
 
     <div class="login-container">
         <div id="output">
-            <p>
-                <em>
             <?php if(isset($form_error)) {
                 foreach ($form_error as $errors) { echo $errors; }
             }?>
-                </em>
-            </p>
         </div>
         <div class="form-box">
             <form action="<?php echo APP_URL; ?>login" method="post">
-                <input name="form[user]" type="text" placeholder="E-mail" style="border-radius: 5px 5px 0 0">
-                <input name="form[password]" type="password" placeholder="Password" style="border-radius: 0 0 5px 5px;">
+                <input class="form-control" name="form[user]" type="text" placeholder="E-mail" style="border-radius: 5px 5px 0 0">
+                <input class="form-control" name="form[password]" type="password" placeholder="Password" style="border-radius: 0 0 5px 5px;">
                 <button class="login_home btn btn-info btn-block login" name="form[action]">Log In</button>
                 <a href="register" class="login_home btn btn-danger btn-block login" >Sign Up</a>
             </form>
--- app/view/user_register.tpl.php
+++ app/view/user_register.tpl.php
@@ -22,42 +22,42 @@ if (isset($form_errors['emailMessage'])) : ?>
     <div class="form-box">
         <form action="<?php echo APP_URL; ?>register" method="post">
             <!--First Name-->
-            <div style="margin-bottom: 25px" class="input-group">
+            <div class="input-group margin_bottom">
                 <span class="input-group-addon">
                     <i class="fa fa-user-circle-o" aria-hidden="true"></i>
                 </span>
                 <input type="text" class="form-control <?php echo ($errFirstName) ? "errorClass" : "" ?>" name="form[firstname]" value="<?php echo $_POST['form']['firstname'];?>" placeholder="First name">
             </div>
             <!--Last Name-->
-            <div style="margin-bottom: 25px" class="input-group">
+            <div class="input-group margin_bottom">
                 <span class="input-group-addon">
                     <i class="fa fa-user-circle" aria-hidden="true"></i>
                 </span>
                 <input type="text" class="form-control <?php echo ($errLastName) ? "errorClass" : "" ?>" name="form[lastname]" value="<?php echo $_POST['form']['lastname'];?>" placeholder="Last name">
             </div>
             <!--E-mail-->
-            <div style="margin-bottom: 25px" class="input-group">
+            <div class="input-group margin_bottom">
                 <span class="input-group-addon">
                     <i class="fa fa-at" aria-hidden="true"></i>
                 </span>
                 <input type="email" class="form-control <?php echo ($errorEmail) ? "errorClass" : "" ?>" name="form[email]" placeholder="E-mail" value="<?php echo $_POST['form']['email'];?>">
             </div>
             <!--Password-->
-            <div style="margin-bottom: 25px" class="input-group">
+            <div class="input-group margin_bottom">
                 <span class="input-group-addon">
                     <i class="fa fa-user-secret" aria-hidden="true"></i>
                 </span>
                 <input type="password" class="form-control <?php echo ($errPassword) ? "errorClass" : "" ?>" name="form[password]" placeholder="Password" value="<?php echo $_POST['form']['password'];?>">
             </div>
             <!--Re password-->
-            <div style="margin-bottom: 25px" class="input-group">
+            <div class="input-group margin_bottom">
                 <span class="input-group-addon">
                     <i class="fa fa-user-secret" aria-hidden="true"></i>
                 </span>
                 <input type="password" class="form-control <?php echo ($errConfirmPass) ? "errorClass" : "" ?>" name="form[confirmPass]" placeholder="Confirm Password" value="<?php echo $_POST['form']['confirmPass'];?>">
             </div>
             <!--Job Name-->
-            <div style="margin-bottom: 25px" class="input-group">
+            <div class="input-group margin_bottom">
                 <span class="input-group-addon">
                     <i class="fa fa-address-card-o" aria-hidden="true"></i>
                 </span>
