6ee24d28bbf621f01afc08c0eda944425cfde03b
Adina Vlasin <adinna.vlasin@yahoo.com>
2017-03-20 18:02:59 +0200

    TI-24 - Refactored user register.

--- app/controller/home.php
+++ app/controller/home.php
@@ -6,7 +6,7 @@
 class controller_home {
 
     /**
-     * This is the homepage.
+     * Generate homepage.
      */
     function action_index() {
         //header('Location: /');
@@ -19,7 +19,7 @@ class controller_home {
      */
     public function action_login() {
         // Checks email and passwordfor validation.
-        if(isset($_POST['form']['action'])) {
+        if (isset($_POST['form']['action'])) {
 
             // Saving form values in variables.
             $login_email = $_POST['form']['user'];
@@ -30,11 +30,10 @@ class controller_home {
 
             // Caching the form errors.
             $form_error = array(
-                'no_email' => empty($login_email) ? "Please insert your email" : FALSE,
-                'no_password' => empty($login_password) ? "Please insert your password" : FALSE,
-                'wrong_email' => ($user_email != $login_email) ? "Wrong e-mail" : FALSE,
-                'wrong_password' => (! empty($login_password) && $user_login->password) ? "Wrong password" : FALSE,
-
+                'no_email' => empty($login_email) ? "Please insert your email!" : FALSE,
+                'no_password' => empty($login_password) ? "Please insert your password!" : FALSE,
+                'wrong_email' => ($user_email != $login_email) ? "Wrong e-mail!" : FALSE,
+                'wrong_password' => (!empty($login_password) && $user_login->password) ? "Wrong password!" : FALSE,
             );
             if ($user_email === $login_email && $user_login->checkPassword($login_password)) {
                 $_SESSION['logged'] = TRUE;
@@ -52,83 +51,48 @@ class controller_home {
      */
     function action_register() {
         $jobs = model_job::getAllJobs();
+        $displayError = FALSE;
 
         if (isset($_POST['btn-register'])) {
             $user_data = array(
-                'lastname' => $_POST['form']['lastname'],
-                'firstname' => $_POST['form']['firstname'],
+                'lastname' => model_user::sanitizeInput($_POST['form']['lastname']),
+                'firstname' => model_user::sanitizeInput($_POST['form']['firstname']),
                 'email' => $_POST['form']['email'],
                 'password' => $_POST['form']['password'],
                 'confirmPassword' => $_POST['form']['confirmPass'],
                 'job' => $_POST['form']['job'],
             );
+
             $form_errors = array(
                 'emailMessage' => '',
+                'limitMessage' => '',
                 'errorEmail' => FALSE,
                 'errorPassword' => FALSE,
                 'errorConfirmPass' => FALSE,
                 'errorLastName' => FALSE,
-                'errorFirstName'  => FALSE,
+                'errorFirstName' => FALSE,
                 'isPasswordNotMatching' => FALSE,
-                );
+            );
 
-            $emailDomain = model_user::validateEmailDomain($user_data['email']);
-            $emailExist = model_user::isEmailRegistered($user_data['email']);
-            $displayError = FALSE;
+            // Check user's lastname and firstname.
+            model_user::validateUserName($form_errors, $user_data, $displayError);
 
-            // Check if user's email exists.
-            if ($emailExist) {
-                $form_errors['emailMessage'] = "This email is already registered.";
-                $displayError = TRUE;
-            }
-            // Check user's email domain.
-            if (!$emailDomain) {
-                $form_errors['emailMessage'] = "Your email should end with @freshbyteinc.com";
-                $form_errors['errorEmail'] = TRUE;
-                $displayError = TRUE;
-            }
-            // Check if passwords match.
-            if ($user_data['password'] != $user_data['confirmPassword']) {
-                $form_errors['isPasswordNotMatching'] = TRUE;
-                $displayError = TRUE;
-                $form_errors['errorPassword'] = TRUE;
-                $form_errors['errorConfirmPass']= TRUE;
-            }
-            // Check if user's lastname is set.
-            if (empty($user_data['lastname'])) {
-                $form_errors['errorLastName'] = TRUE;
-                $displayError = TRUE;
-            }
-            // Check if user's firstname is set.
-            if (empty($user_data['firstname'])) {
-                $form_errors['errorFirstName'] = TRUE;
-                $displayError = TRUE;
-            }
-            // Check if user's email is set.
-            if (empty($user_data['email'])) {
-                $form_errors['errorEmail'] = TRUE;
-                $displayError = TRUE;
-            }
-            // Check if user's password is set.
-            if (empty($user_data['password'])) {
-                $form_errors['errorPassword'] = TRUE;
-                $displayError = TRUE;
-            }
-            // Check if user's confirm password is set.
-            if (empty($user_data['confirmPassword'])) {
-                $form_errors['errorConfirmPass'] = TRUE;
-                $displayError = TRUE;
-            }
+            // Check user's email.
+            model_user::validateUserEmail($form_errors, $user_data, $displayError);
+
+            // Check user's password and user's confirm password.
+            model_user::validatePassword($form_errors, $user_data, $displayError);
+
             // If there are no errors displayed, attempt to add the user.
             if (!$displayError) {
                 try {
                     $user = model_user::addUser(
-                    $user_data['lastname'],
-                    $user_data['firstname'],
-                    $user_data['email'],
-                    $user_data['password'],
-                    $user_data['job']
-                );
+                        $user_data['lastname'],
+                        $user_data['firstname'],
+                        $user_data['email'],
+                        $user_data['password'],
+                        $user_data['job']
+                    );
                     header('Location: /home/login');
                 } catch (Exception $e) {
                     header('Location: /500/index');
@@ -145,10 +109,11 @@ class controller_home {
      */
     function action_track() {
         if (!isset($_SESSION['logged']) || !$_SESSION['logged']) {
-            $_SESSION['logged'] = false;
+            $_SESSION['logged'] = FALSE;
             header('Location: /home/login');
-        } else {
-            $_SESSION['logged'] = true;
+        }
+        else {
+            $_SESSION['logged'] = TRUE;
             // Include view for this page.
             @include_once APP_PATH . 'view/track_page.tpl.php';
         }
