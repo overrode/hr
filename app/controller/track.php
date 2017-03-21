98b2746091e7408765dd188d427ea3ee453e9c3c
Adina Vlasin <adinna.vlasin@yahoo.com>
2017-03-21 17:28:23 +0200

    TI-4 Initial change add function.

--- app/controller/track.php
+++ app/controller/track.php
@@ -13,12 +13,43 @@ class controller_track {
         @include_once APP_PATH . 'view/work_page.tpl.php';
     }
 
-    public function action_getDate($id) {
+    public function action_getDate() {
         $get_work = model_work::getWork(1);
-        print json_encode($get_work);
+        return json_encode($get_work);
     }
 
+    /**
+     * Add a work.
+     */
+    public function action_add() {
+        $displayError = FALSE;
+        if (isset($_POST['submit_work'])) {
 
+            $project_data = array(
+                'project' => $_POST['project'],
+                'task' => $_POST['task'],
+                'hours' => $_POST['hours'],
+                'details' => $_POST['details'],
+            );
+
+            model_user::validateInput($form_errors, $project_data, $displayError);
+           // if (!$displayError) {
+                try {
+                    $work = model_work::createWork(
+                        "2017-12-12 12:12:12",
+                        $project_data['project'],
+                        $project_data['task'],
+                        $project_data['hours'],
+                        $project_data['details'],
+                        $_SESSION['id']
+                    );
+                    header('Location: /track/index');
+                } catch (Exception $e) {
+                    header('Location: /500/index');
+                }
+          //  }
+        }
+    }
 
 }
 
