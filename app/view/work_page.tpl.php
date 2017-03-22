d158130e955fce633fb7849e6bf504c264445a31
Adina Vlasin <adinna.vlasin@yahoo.com>
2017-03-22 15:59:52 +0200

    TI-33 Calendar functionality.

--- app/view/work_page.tpl.php
+++ app/view/work_page.tpl.php
@@ -2,19 +2,19 @@
 
     <main class="container track_bg">
         <div class="row">
-            <div class="col-lg-12 col-md-12- colxs-12">
-                <div class="col-md-6" id='calendar'></div>
-                <div class="col-md-6">
-                    <form action="" method="post">
+            <div class="col-lg-12 col-md-12- col-xs-12">
+                <div class="col-md-6 col-xs-6" id='calendar'></div>
+                <div class="col-md-6 col-xs-6">
+                    <form action="<?php echo APP_URL; ?>/track/add" method="post">
                         <!--Project-->
                         <label>Project</label>
                         <div class=" margin_bottom">
-                            <input id="form_project" type="text" class="form-control" name="project" value=""  rows="3">
+                            <input id="form_project" type="text" class="form-control" name="project" value="">
                         </div>
                         <!--Task-->
                         <label>Task</label>
                         <div class=" margin_bottom">
-                            <input id="form_task" type="text" class="form-control" name="task" value="" rows="3">
+                            <input id="form_task" type="text" class="form-control" name="task" value="">
                         </div>
                         <!--Details-->
                         <label>Details</label>
@@ -29,7 +29,7 @@
                         <!--Date-->
                         <label>Date</label>
                         <div class=" margin_bottom">
-                            <input id="form_date" type="date" class="form-control"  name="date"  value="" disabled >
+                            <input id="form_date" type="text" class="form-control"  name="date"  value="" disabled >
                         </div>
                         <button class="login_home btn btn-info btn-block login" name="submit_work">SAVE</button>
                     </form>
@@ -43,7 +43,6 @@
                         <th>Task</th>
                         <th>Hours</th>
                         <th>Details</th>
-                        <th>Date</th>
                     </tr>
                     </thead>
                     <tbody id="tbody">
--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,37 @@
 /* Custom javascript */
 
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     /*Calendar options*/
     $('#calendar').fullCalendar({
         weekends: false,
         defaultFormat: 'YYYY-MM-DD',
-        dayClick: function(date) {
+        dayClick: function (date) {
             var datePicker = date.format();
+            $('#form_date').val(datePicker);
             $.ajax({
                 type: "POST",
                 dataType: "json",
                 url: "/track/getDate",
-                data: {data:datePicker},
-                success: function(response){
+                data: {data: datePicker},
+                success: function (response) {
                     $('#tbody').empty();
-                    $.each(response, function(index, val) {
+                    $.each(response, function (index, val) {
                         var eachrow = "<tr>"
                             + "<td>" + val.project + "</td>"
                             + "<td>" + val.task + "</td>"
                             + "<td>" + val.hours + "</td>"
                             + "<td>" + val.details + "</td>"
-                            + "<td>" + val.date + "</td>"
+                            + "<button>Edit</button>" +
                             + "</tr>";
                         $('#tbody').append(eachrow);
                     });
                 },
-                error: function(err) {
+                error: function (err) {
                     alert(err);
                 },
-
             });
         },
-
     });
-
 });
 
 
